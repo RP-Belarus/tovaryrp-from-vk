934cc3115fe91af2608f26c82342324be6641dee
dimitrius-brest <d_der@mail.ru>
2021-02-22 16:43:50 +0300

    Обновлены get-запросы к api ВК

--- index.js
+++ index.js
@@ -17,20 +17,48 @@ app.use(express.json({ limit: '1mb'}));
 // Токен получен для localhost:3000
 // "expires_in": 0  (бесконечное время жизни токена)
 const access_token = process.env.ACCESS_TOKEN;    // Получает токен из файла .env
-
-const tovar_id = "-93793008_964938"; // яблоки
-//const tovar_id = "-93793008_79152";  // посуда из глины
 const api_version = "5.126";
-const tovar_url = "https://api.vk.com/method/market.getById?item_ids=" + tovar_id +
-        "&access_token=" + access_token + "&v=" + api_version;
 
-app.get('/tovar', async (request, response) => {
-    const fetch_response = await fetch(tovar_url);
-    const json = await fetch_response.json();
+//const tovar_id = "-93793008_964938"; // яблоки
+//const tovar_id = "-93793008_79152";  // посуда из глины
+//const tovar_url = "https://api.vk.com/method/market.getById?item_ids=" + tovar_id +
+//        "&access_token=" + access_token + "&v=" + api_version;
+
+//app.get('/tovar', async (request, response) => {
+//    const fetch_response = await fetch(tovar_url);
+//    const json = await fetch_response.json();
     //console.log(json);
     //console.log("Count: " + json.response.count);
     //console.log("Title: " + json.response.items[0].title);
     //console.log("Description: " + json.response.items[0].description);
+//    response.json(json);
+//});
+
+
+// Берёт id товара из строки запроса, возвращает товар из API Вконтакта
+// Пример строки: /tovar?id=-93793008_964938
+app.get('/tovar', async (request, response) => {
+    const id = request.query.id;
+    const url = "https://api.vk.com/method/market.getById?item_ids=" + id +
+        "&access_token=" + access_token + "&v=" + api_version;
+    const fetch_response = await fetch(url);
+    const json = await fetch_response.json();
+    response.json(json);
+});
+
+// Берёт id продавца из строки запроса, возвращает товары продавца из API Вконтакта
+// Пример строки: /tovary?owner_id=-93793008
+// Примеры owner_id:
+// -93793008  Райский сад
+// -128194899  Поместье Огородников
+// -81509275  Мастерская деревянной живописи "ПинескЪ"
+// -13916738  Мастерская по дереву "Добрый лес"
+app.get('/tovary', async (request, response) => {
+    const owner_id = request.query.owner_id;
+    const url = "https://api.vk.com/method/market.get?owner_id=" + owner_id +
+        "&access_token=" + access_token + "&v=" + api_version;
+    const fetch_response = await fetch(url);
+    const json = await fetch_response.json();
     response.json(json);
 });
 
